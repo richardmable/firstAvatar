0a06461b72c768d18ca8b91ce6a0dbcb473145db
Richard Mable <richardbmable@gmail.com>
2016-01-14 10:57:43 -0500

    users can sign up and upload an avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,34 @@
 class UsersController < ApplicationController
   def index
+    @user = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to users_path
+      flash[:notice] = "Account creation was a great success!"
+    else
+      flash[:alert] = "Account could not be created."
+      redirect_to users_path
+    end
   end
 
   def show
   end
 
-  def create
-  end
+
 
   def destroy
   end
 
-  def new
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :avatar)
   end
+
 end
